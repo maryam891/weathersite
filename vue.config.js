60129a5deb8c88d22f6186e4f0e7f7eb60492632
maryam008@hotmail.se <>
2021-03-04 20:19:38 +0100

    fix public path

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/Weathersite-project/'
+    : '/',
     pwa: {
         background_color: 'lightblue',
         manifestOptions: {
