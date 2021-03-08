8dadf08acf76e990c0b74ea6f9f2ff160965dc07
maryam008@hotmail.se <>
2021-03-08 20:39:39 +0100

    base url fixes

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/Weathersite-project/'
+    ? '/Weathersite-project/#/'
     : '/',
     pwa: {
         background_color: 'lightblue',
