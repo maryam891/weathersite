0809e08733f06121ab72c41b716a615c868a6a3f
maryam008@hotmail.se <>
2021-03-13 13:53:17 +0100

    initial dist subtre commit

---
+++ dist/precache-manifest.70dc72077c4e8f79915d6a6b4a02acb0.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1842e9b6bbf3f3e4329",
+    "url": "weathersite/css/app.b6ae0a74.css"
+  },
+  {
+    "revision": "8a8e3c0e73a8df9acfee",
+    "url": "weathersite/css/chunk-vendors.64d43e69.css"
+  },
+  {
+    "revision": "53b3217dd00b8814b84f732df31fe78f",
+    "url": "weathersite/index.html"
+  },
+  {
+    "revision": "e1842e9b6bbf3f3e4329",
+    "url": "weathersite/js/app.5e7a1616.js"
+  },
+  {
+    "revision": "8a8e3c0e73a8df9acfee",
+    "url": "weathersite/js/chunk-vendors.5095a7fd.js"
+  },
+  {
+    "revision": "20e4a16d622888f5e3fab06c5ab82e87",
+    "url": "weathersite/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "weathersite/robots.txt"
+  },
+  {
+    "revision": "133f50776a6809127ab2ff15ee9a0082",
+    "url": "weathersite/sky.jpg"
+  }
+]);
